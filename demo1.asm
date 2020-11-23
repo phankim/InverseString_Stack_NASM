2fa280e2666d54aec8912c5e22dcda130161aba3
Phan Thi Hai Ha <phanha.leti@gmail.com>
2020-11-23 16:35:01 -0500

    Reup to save
    Thanks to source code of 0xAx.
    github/0xAx/asm/blob/master/strings/reverse.asm

--- demo1.asm
+++ demo1.asm
@@ -1,65 +1,66 @@
-
 section .data
-    prompt db "Enter string: "
-    length equ $ -prompt
-    text times 255 db 0
-    buffer times 255 db 0
-section .text 
-    global _start
+	SYS_WRITE	equ 1
+	STD_OUT		equ 1
+	SYS_EXIT	equ 60
+	EXIT_CODE	equ 0
+	NEW_LINE	db 0xa
+	;Define Data - - - - - - - - - - 
+	Input db "SPB_ETU",10
+	length equ $ -Input
+section .bss
+    OUTPUT resb 1
+section .text
+	global _start
 _start:
-        ; in ra chuoi vua nhap
-        mov rax,1;system call for writing
-        mov rdi,1;file handle 1 is stdout (stdout has a file descriptor of 1)
-        mov rsi,prompt;address of our input string prompt
-        mov rdx,length;number of length string's
-        syscall;invoke OS to do the write
-        
-        mov rax,0   
-        mov rdi,0
-        mov rsi,text;ham de nhap string input
-        syscall
+	;print input string
+	mov rax, SYS_WRITE
+	mov rdi, STD_OUT
+	mov rsi, Input
+	mov rdx, length
+	syscall
 
-        mov rcx,rax   ;rcx la ky tu counter
-        mov rsi,text; contro  tro den ky tu hien tai, bat dau 
-        add rsi,rcx;dem de tro
-        dec rsi;nho chi so 0
-        mov rdi,buffer
-
-    ; ; Loop to put characters into stack
-    ;     mov rax,dword[prompt]
-    ;     mov rbx,text
-    ;     mov r12,0
-    ;     mov rax,0
-    ; pushLoop:
-    ;     push dword[rbx+r12*8]
-    ;     inc r12
-    ;     loop pushLoop
-    ; ; ;All characters are on stack (in reverse order)
-    ; ; ;Loop to get them back off, put them back into the original list
-    ;     mov rcx,dword[prompt]
-    ;     mov rbx,text
-    ;     mov r12,0
-    ;     loop pushLoop
-    ; popLoop:
-    ;     pop rax
-    ;     mov dword[rbx+r12*8],rax
-    ;     inc rdi
-    ;     loop popLoop
-
-    ; process_loop:
-    ;     mov bl,[rsi];copy back to prompt
-    ;     mov [rdi],bl
-    ;     inc rdi
-    ;     dec rsi
-    ;     dec rax
-    ;     jnz process_loop
-
-        mov eax,1;system call for writing
-        mov edi,1;ile handle 1 is stdout (stdout has a file descriptor of 1)
-        mov esi,buffer;address of our input string buffer
-        mov edx,ecx
-        syscall
-    exit:
-        mov rax,60;system call for exitting
-        mov rdi,0;exit code =0
-        syscall
+	mov rsi,Input ;get address of Input
+	xor rcx,rcx ;zeroize rcx for counter
+	cld ; df=0 si++
+	mov rdi,$+15
+	call StrLen ; call function to calculate string's length
+	xor rax,rax ; write 0 to rax
+	xor rdi,rdi ; additional counter for reverseStr
+	jmp reverseStr ;jump to function reverseStr
+StrLen:
+	cmp byte[rsi],0 ;check if it is end of the string
+	je exitCheck ;yes - so exit from loop
+	lodsb ; load byte from rsi to al and increase rsi
+	push rax ;push symbol to stack
+	inc rcx
+	jmp StrLen ;Loop again
+exitCheck:
+	;push return address to stack again
+	push rdi 
+	ret ;return to start
+reverseStr:
+	cmp rcx,0;check if it is end of string
+	je printResult ;if yes print string
+	pop rax ;pop symbol from stack
+	mov [OUTPUT+rdi],rax ;write it to output buffer
+	dec rcx
+	inc rdi
+	jmp reverseStr
+printResult:
+	mov rdx,rdi
+	mov rax, 1
+	mov rdi, 1
+	mov rsi,OUTPUT
+	syscall
+	jmp printNewLine
+printNewLine:
+	mov rax,SYS_WRITE
+	mov rdi,STD_OUT
+	mov rsi,NEW_LINE
+	mov rdx,1
+	syscall
+	jmp _exit
+_exit:
+	mov rax, SYS_EXIT
+	mov rdi, EXIT_CODE
+	syscall
